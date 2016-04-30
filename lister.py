b9ce097d1a2250868935cde6c922d281bdae0dec
tishmen <topuzov.work@gmail.com>
2016-05-01 01:28:33 +0200

    fix

--- lister.py
+++ lister.py
@@ -158,9 +158,9 @@ def get_ebay_category(product):
 
 def get_ebay_image(product):
     path = os.path.join(IMAGE_DIR, product['image'])
-    return_val = [IMAGE_URL.format(product['image'])]
+    url = IMAGE_URL.format(product['image'])
     if os.path.exists(path):
-        return return_val
+        return url
     response = requests.get(
         'http://ecx.images-amazon.com/images/I/{}'.format(product['image'])
     )
@@ -175,7 +175,7 @@ def get_ebay_image(product):
     white = Image.new('RGBA', (500, 500), (255, 255, 255, 255))
     white.paste(image, (x, y))
     white.save(path)
-    return return_val
+    return url
 
 
 def get_ebay_html(product, title):
@@ -209,9 +209,7 @@ def list_ebay_product(product):
             'Location': 'Los Angeles, CA',
             'PaymentMethods': 'PayPal',
             'PayPalEmailAddress': 'joshwardini@gmail.com',
-            'PictureDetails': {
-                'PictureURL': get_ebay_image(product)
-            },
+            'PictureDetails': {'PictureURL': get_ebay_image(product)},
             'ItemSpecifics': {
                 'NameValueList': [
                     {'Name': 'Brand', 'Value': product['brand']},
